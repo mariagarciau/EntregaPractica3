1a324ce04a05e0143e77d363d67ec341ec133f53
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-25 11:50:50 +0200

    funciona

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,2 +1,2 @@
 #Realiza el psedocódigo y un desarrollo en Python para calcular la distancia recorrida (m) por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que es un MRU (Movimiento Rectilíneo Uniforme).
- 
+print("hola")
