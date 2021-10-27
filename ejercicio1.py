d8fab23595595db0b4b7df7ccaecb39db4d95c78
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 11:05:17 +0200

    enunciado

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,4 +1,3 @@
-#Realiza el psedocódigo y un desarrollo en Python para calcular la distancia recorrida (m) por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que es un MRU (Movimiento Rectilíneo Uniforme).
-print("hola")
-print("adios")
-print("me llevo mal con lso ordenadoesws")
+#Realiza el psedocódigo y un desarrollo en Python para calcular la distancia recorrida (m)
+# por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que
+# es un MRU (Movimiento Rectilíneo Uniforme).
