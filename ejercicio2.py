5d144b4c3a95a3128572b9f2e30d277793d83014
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 12:10:15 +0200

    primera prueba

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,7 +1,12 @@
 #Realiza el psedocódigo y un desarrollo en Python para obtener el promedio simple
 # de un estudiante a partir de sus tres notas parciales N1, N2 y N3.
-
 N1 : float
 N2 : float
 N3 : float
-promedio= (N1+N2+N3)/2
+print("Introduzca la primera nota")
+input(N1)
+print("Introduzca la segunda nota")
+input(N2)
+print("Introduzca la tercera nota")
+input(N3)
+print(promedio= (N1+N2+N3)/3)
