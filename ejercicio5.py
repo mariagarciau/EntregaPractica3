c43c54cd2b1443f17da00a79286fdf4c37e450ee
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 14:13:25 +0200

    funciona

--- ejercicio5.py
+++ ejercicio5.py
@@ -3,3 +3,11 @@
 # dispensadores de esta registran lo que “surten” en galones, pero
 # el precio de la gasolina está fijado en litros. El desarrollo debe
 # calcular e imprimir lo que hay que cobrarle al cliente.
+
+#Como no nos dan el precio, supongo que son 0.965€ el litro
+precio = float(0.965)
+galones = float(input("Introduce los galones que ha echado a su depósito: "))
+#1 galón son 3.78541 litros
+litros = galones*3.78541
+precio *=litros
+print(precio)
