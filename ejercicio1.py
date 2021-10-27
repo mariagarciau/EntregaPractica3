d9054361385b320651b9976fa312e1cbaedb5d3e
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 13:31:18 +0200

    funciona

--- ejercicio1.py
+++ ejercicio1.py
@@ -1,3 +1,10 @@
 #Realiza el psedocódigo y un desarrollo en Python para calcular la distancia recorrida (m)
 # por un móvil que tiene velocidad constante (m/s) durante un tiempo t (s), considerar que
 # es un MRU (Movimiento Rectilíneo Uniforme).
+distancia : float
+velocidad = float(input("Introduce la velocidad en m/s que lleva: "))
+tiempo = float(input("Introduce el tiempo en segundos que ha tardado: "))
+#Suponiendo que la distancia inicial recorrida es de 0m, calculamos la distancia
+#total recrrida como distancia = velocidad*tiempo
+distancia = velocidad*tiempo
+print(distancia)
