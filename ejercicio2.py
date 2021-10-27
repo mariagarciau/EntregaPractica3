f27a2a2003bc01d15e4e2a9e661a1e5c535b0443
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 11:30:42 +0200

    declaracion variables

---
+++ ejercicio2.py
@@ -0,0 +1,7 @@
+#Realiza el psedocódigo y un desarrollo en Python para obtener el promedio simple
+# de un estudiante a partir de sus tres notas parciales N1, N2 y N3.
+
+N1 : float
+N2 : float
+N3 : float
+promedio= (N1+N2+N3)/2
