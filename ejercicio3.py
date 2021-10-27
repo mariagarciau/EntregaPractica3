2eea97cdecd05aae13935e6066db5b777e806334
mariagarciau <91331262+mariagarciau@users.noreply.github.com>
2021-10-27 13:22:44 +0200

    funciona

--- ejercicio3.py
+++ ejercicio3.py
@@ -3,3 +3,10 @@
 #correspondientes a un kahoot!, y muestre su puntaje final considerando que
 #por cada respuesta correcta tendrá 3 puntos, respuestas incorrectas tendrá -1
 #y respuestas en blanco tendrá 0.
+correctas = int(input("Introduce el número de respuestas correctas: "))
+incorrectas = int(input("Introduce el número de repuestas incorrectas: "))
+blanco = int(input("Introduce el número de respuestas en balnco: "))
+
+totalPuntos : int
+totalPuntos = correctas*3 + incorrectas*-1 + blanco*0
+print(totalPuntos)
